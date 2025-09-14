a3fa04e8a84375f90c14a2d9035bf272e03627f5
Zacquie <zacquiecandeeze@gmail.com>
2025-09-14 13:56:56 -0700

    Initial Commit

---
+++ LatchFitness/LatchFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatchFitnessApp.swift
+//  LatchFitness
+//
+//  Created by Proxy on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct LatchFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
